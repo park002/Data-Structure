ffb72bd396d79b87819608c5c51bafa831ef49f5
pjh <ekem159@naver.com>
2020-06-17 20:32:14 +0900

    String Test

---
+++ DataStructure/src/StringTest/StringTest2.java
@@ -0,0 +1,11 @@
+package StringTest;
+
+public class StringTest2 {
+	public static void main(String[] args) {
+		String abc = "abc";
+		for (int i = 0; i < abc.length(); i++) {
+			System.out.println(abc.charAt(i));
+		}
+
+	}
+}
